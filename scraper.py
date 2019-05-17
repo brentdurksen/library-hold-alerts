5f4672ff3d4c758e772f94e8aa4fdd09de556e53
Brent Durksen <brentd@momentum.org>
2019-05-16 18:34:52 -0600

    Determine hold positions

--- scraper.py
+++ scraper.py
@@ -20,5 +20,11 @@ br.form['name'] = datastore["username"]
 br.form['user_pin'] = datastore["pin"] 
 br.submit()
 
-print br.response().read()
+soup = BeautifulSoup(br.response().read(), "lxml")
 
+for row in soup.find_all("div", "item-row item-header"):
+    print(row.find("span", "title-content").get_text())
+    print(row.find("span", "cp-subtitle").get_text())
+    hold_position = row.find("span", "cp-hold-position")
+    position_of_last_book = hold_position.strong.get_text()
+    print("Position: " + position_of_last_book)
